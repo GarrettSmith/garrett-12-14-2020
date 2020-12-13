7e412f2cc4612fd9d8c2d364176095a2eaae4c18
GarrettSmith <garrettwhsmith@gmail.com>
2020-12-13 14:02:14 -0600

    use diesign filesize format

--- client/src/common/format.ts
+++ client/src/common/format.ts
@@ -1,5 +1,8 @@
 import numeral from "numeral";
 import { filesizeFormat } from "./constants";
 
+// Format to lowercase to match designs
+// Normally, I would have raised this concern with design, 
+// or follow company standards around displaying filesize.
 export const formatFilesize = (size: number) =>
-  numeral(size).format(filesizeFormat);
+  numeral(size).format(filesizeFormat).toLowerCase();
